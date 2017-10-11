cbadccc1d7d58780b6ab549272037bba58f06cf0
oppoi <oppoi@wow.com>
2017-10-11 21:03:47 +0200

    typo fixed, more comments added

--- pubgscraper.py
+++ pubgscraper.py
@@ -1,22 +1,22 @@
 #python3
 """ 
 A pubg inventational web scraper, that checks if you invite button is still 'Opening Soon'.
-If the button changes it will send an E-Mail to your adressee.
+If the button changes, it will send an E-Mail to your adressee.
 """
 
 import requests, bs4, time, smtplib
 
-#always true -> checks all the time, will use timesleep later
+#always true -> loop to check if button is still the same.
 while True:
     url = "http://en.intelextrememasters.com/season-12/oakland/pubg/"
     #imitate a browser
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
     # download the homepage
-    res = requests.get(url)
+    request_url = requests.get(url)
     #downloads the website
-    page = bs4.BeautifulSoup(res.text)
+    request_page = bs4.BeautifulSoup(request_url.text)
 
-    elems = page.select('.pubg_button')
+    elems = request_page.select('.pubg_button')
     
     if elems[0].getText() == "SIGN UP FOR EU - OPENING SOON!":
         print("Attempting...")
@@ -27,6 +27,7 @@ while True:
         from_email = 'your_email'
         to_email = 'to_email'
 
+        #server settings to send your email.
         server = smtplib.SMTP('smtp.mail.yahoo.com', 587)
         server.starttls()
         server.login("your_email", "your_pw")
