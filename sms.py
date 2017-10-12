955583956066f3ae0d3ed3fb9d8fb2872ed40d23
Oppoi1 <oppoi@wow.com>
2017-10-12 08:43:40 +0200

    Update sms.py

--- sms.py
+++ sms.py
@@ -4,7 +4,7 @@ from twilio.rest import Client
 
 def sms():
     #twilio Account SID and Auth Token
-    client = Client("ACf24b3bdc844d344e78195d568f618f2b", "37ba6ef47864fe8f7c5bd6ed12b9172d")
+    client = Client("Account SID", "Auth Token")
 
     client.messages.create(to="+number",
                         from_="+number",
